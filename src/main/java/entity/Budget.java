9f2f7e5ef151a1653a8ce751faa7f015f692cfc9
Hesselbacher, Bryce <bhesselbacher@madisoncollege.edu>
2024-03-13 13:56:53 -0500

    Added code to Budget.java.

--- src/main/java/entity/Budget.java
+++ src/main/java/entity/Budget.java
@@ -1,4 +1,37 @@
 package entity;
 
+import jakarta.persistence.*;
+import java.util.*;
+import org.hibernate.annotations.GenericGenerator;
+
+/**
+ * A class to represent a budget.
+ *
+ * @author bhesselbacher
+ */
+@Entity
+@Table(name = "budget")
 public class Budget {
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO, generator="native")
+    @GenericGenerator(name = "native",strategy = "native")
+    private int id;
+
+    @OneToMany(mappedBy = "budget", cascade = CascadeType.ALL, orphanRemoval = true,
+            fetch = FetchType.EAGER)
+    private List<Month> months = new ArrayList<>();
+
+    @OneToMany(mappedBy = "budget", cascade = CascadeType.ALL, orphanRemoval = true,
+            fetch = FetchType.EAGER)
+    private List<BudgetType> budgetTypes = new ArrayList<>();
+
+    @Column
+    private double amount;
+
+    public Budget() {
+    }
+
+    public Budget(double amount) {
+        this.amount = amount;
+    }
 }
